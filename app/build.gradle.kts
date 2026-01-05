25a055ee91f826e668ae87824eca761d4b4951d7
urgangur <pinjaehuang@gmail.com>
2026-01-06 02:28:48 +0800

    pizza

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
-    kotlin("android") version "2.0.21"
-    id("org.jetbrains.kotlin.android.compose") version "2.0.21"
+    id("org.jetbrains.kotlin.android") version "2.0.21"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
 }
 
 android {
